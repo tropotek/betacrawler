e6bc2ed0f3df1f6ece24f1204c76eb22d8bedc2a
tropotek <info@tropotek.com>
2021-08-13 10:51:12 +1000

    c

--- src/Settings/Settings.cpp
+++ src/Settings/Settings.cpp
@@ -1,19 +1,14 @@
 /*
- * Copyright (C) 2020  www.tropotek.com
+ * Copyright (C) 2021  www.tropotek.com
  * Project: Betacrawler
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
+ * Facebook: https://www.facebook.com/groups/307432330496662/
+ * Github: https://github.com/tropotek/betacrawler 
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * ***********************************************************************
+ * 
  */
 #include "Settings.h"
 
